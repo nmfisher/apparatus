f813cbe71fb37d39c6afc08aca02b90577f3cdf4
Nick Fisher <nick.fisher@avinium.com>
2017-06-09 07:57:59 +0100

    add tests

--- spec/randomforest_classifier_spec.js
+++ spec/randomforest_classifier_spec.js
@@ -23,74 +23,132 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 var RandomForestClassifier = new require('../lib/apparatus/classifier/randomforest_classifier');
 
-var randomforest = new RandomForestClassifier();
-		    randomforest.addExample([1.5    , 1.5       ], 0);
-		    randomforest.addExample([-1.5 ,   -1.5 ], 1);
-		    randomforest.addExample([1000,   2000  ], 2);
-
-		    randomforest.train();
-		    randomforest.classify([0.5 , 0.5 ])
-
 describe('randomforest', function() {
-	     it('should perform binary classifcation', function() {
-		    var randomforest = new RandomForestClassifier();
+    it('should perform binary classification with 2d features', function() {
+        var randomforest = new RandomForestClassifier();
 
-		    randomforest.addExample([-0.4326,  1.1909   ], 1);
-		    randomforest.addExample([1.5    , 3.0       ], 1);
-		    randomforest.addExample([0.1253 , -0.0376   ], 1);
-		    randomforest.addExample([0.2877 ,   0.3273  ], 1);
-		    randomforest.addExample([-1.1465,   0.1746  ], 1);
-		    randomforest.addExample([1.8133 ,   2.1139  ], -1);
-		    randomforest.addExample([2.7258 ,   3.0668  ], -1);
-		    randomforest.addExample([1.4117 ,   2.0593  ], -1);
-		    randomforest.addExample([4.1832 ,   1.9044  ], -1);
-		    randomforest.addExample([1.8636 ,   1.1677  ], -1);
+        randomforest.addExample([-0.4326,  1.1909   ], 1);
+        randomforest.addExample([1.5    , 3.0       ], 1);
+        randomforest.addExample([0.1253 , -0.0376   ], 1);
+        randomforest.addExample([0.2877 ,   0.3273  ], 1);
+        randomforest.addExample([-1.1465,   0.1746  ], 1);
+        randomforest.addExample([1.8133 ,   2.1139  ], -1);
+        randomforest.addExample([2.7258 ,   3.0668  ], -1);
+        randomforest.addExample([1.4117 ,   2.0593  ], -1);
+        randomforest.addExample([4.1832 ,   1.9044  ], -1);
+        randomforest.addExample([1.8636 ,   1.1677  ], -1);
 
-		    randomforest.train();
-
-		    expect(randomforest.classify([-0.5 , -0.5 ])).toBe(1);
-
-		    // random forest are not deterministic, check on average it works
-		    var count = 0;
-		    for(var tests=0; tests<200; tests++){
-			randomforest.train();
-        		if(randomforest.classify([1.0, 2.0]) == 1) {
-			    count++;
-			}
-		    }
-		    expect(count).toBeGreaterThan(50);
-		});
-  it('should perform multiclass classifcation', function() {
-		    var randomforest = new RandomForestClassifier();
-
-		    randomforest.addExample([-0.4326,  1.1909   ], 0);
-		    randomforest.addExample([1.5    , 3.0       ], 0);
-		    randomforest.addExample([0.1253 , -0.0376   ], 0);
-		    randomforest.addExample([0.2877 ,   0.3273  ], 0);
-		    randomforest.addExample([-1.1465,   0.1746  ], 0);
-		    randomforest.addExample([1.8133 ,   2.1139  ], 1);
-		    randomforest.addExample([2.7258 ,   3.0668  ], 1);
-		    randomforest.addExample([1.4117 ,   2.0593  ], 1);
-		    randomforest.addExample([4.1832 ,   1.9044  ], 1);
-		    randomforest.addExample([1.8636 ,   1.1677  ], 1);
-         randomforest.addExample([-100.8133 ,   -200.1139  ], 2);
-		    randomforest.addExample([-200.7258 ,   -30.0668  ], 2);
-		    randomforest.addExample([-140.117,   -200.593  ], 2);
-		    randomforest.addExample([-4.1832 ,   -1.9044  ], 2);
-		    randomforest.addExample([-1.8636 ,   -1.1677  ], 2);
-
-		    randomforest.train();
-
-		    expect(randomforest.classify([-0.5 , -0.5 ])).toBe(0);
-
-		    // random forest are not deterministic, check on average it works
-		    var count = 0;
-		    for(var tests=0; tests<200; tests++){
         randomforest.train();
-              if(randomforest.classify([1.0, 2.0]) == 1) {
+
+        expect(randomforest.classify([-0.5 , -0.5 ])).toBe(1);
+
+        // random forest are not deterministic, check on average it works
+        var count = 0;
+        for(var tests=0; tests<100; tests++){
+            randomforest.train();
+            if(randomforest.classify([1.0, 2.0]) == 1) {
                 count++;
-              }
+            }
+        }
+        expect(count).toBeGreaterThan(50);
+    });
+
+    it('should perform multiclass classification with 3d features', function() {
+        var randomforest = new RandomForestClassifier();
+
+        // generated from sklearn.datasets.make_classification
+        var train_examples = [
+            [ 0.8080942 ,  1.30614285, -1.35468264],
+            [ 2.77361167, -0.72931586, -1.29065071],
+            [-0.49200143,  1.24270065,  2.13055164],
+            [ 4.66522504, -1.04715041, -0.50329974],
+            [-1.8997677 ,  1.06832484, -0.71905057],
+            [-0.97324051, -0.53861337,  0.83167627],
+            [-0.89175073, -0.8998025 ,  0.9771675 ],
+            [-0.71374886,  1.95640935,  0.21539975],
+            [-1.13796921, -1.17133199,  1.09793521],
+            [-0.73763646,  4.46314491,  3.14618865],
+            [-1.78924113,  0.82935002, -0.74527505],
+            [-0.97522451, -1.56499373,  1.12782555],
+            [ 1.80333287,  0.77424192, -1.52114292],
+            [-1.05755709, -0.37600237, -1.16263587],
+            [ 1.20867444,  3.60765148, -0.23910699]]
+
+        var test_examples = [
+            [-1.81077556,  0.34670694,  0.46382542],
+            [ 0.8668284 ,  2.67468251, -0.08890863],
+            [ 0.75710073,  1.64514855, -1.49304881],
+            [-0.08449747,  1.05468084,  2.74085508],
+            [-1.08888363, -1.94654662,  0.42889803]]
+
+        var train_labels = [0, 0, 2, 0, 2, 1, 1, 1, 1, 2, 1, 0, 1, 0]
+        var test_labels = [2, 0, 0, 2, 1]
+
+        for(var i = 0; i < train_examples.length; i++) {
+          randomforest.addExample(train_examples[i], train_labels[i]);
+        }
+
+        randomforest.train();
+        for(var i = 0; i < test_examples.length; i++) {
+          expect(randomforest.classify(test_examples[i])).toBe(test_labels[i])
+        }
+
+        // random forest are not deterministic, check on average it works
+        var count = 0;
+        for(var tests=0; tests<100; tests++){
+            randomforest.train();
+            if(randomforest.classify([1.0, 2.0, 3.0]) == 0) {
+                count++;
+            }
+        }
+        expect(count).toBeGreaterThan(50);
+    });
+
+  it('should perform multiclass classification with 3d features & 2D (dot product) decisions', function() {
+        var randomforest = new RandomForestClassifier({type:1});
+
+        // generated from sklearn.datasets.make_classification
+        var train_examples = [
+            [ 0.8080942 ,  1.30614285, -1.35468264],
+            [ 2.77361167, -0.72931586, -1.29065071],
+            [-0.49200143,  1.24270065,  2.13055164],
+            [ 4.66522504, -1.04715041, -0.50329974],
+            [-1.8997677 ,  1.06832484, -0.71905057],
+            [-0.97324051, -0.53861337,  0.83167627],
+            [-0.89175073, -0.8998025 ,  0.9771675 ],
+            [-0.71374886,  1.95640935,  0.21539975],
+            [-1.13796921, -1.17133199,  1.09793521],
+            [-0.73763646,  4.46314491,  3.14618865],
+            [-1.78924113,  0.82935002, -0.74527505],
+            [-0.97522451, -1.56499373,  1.12782555],
+            [ 1.80333287,  0.77424192, -1.52114292],
+            [-1.05755709, -0.37600237, -1.16263587],
+            [ 1.20867444,  3.60765148, -0.23910699]]
+
+        var test_examples = [
+            [-1.81077556,  0.34670694,  0.46382542],
+            [ 0.8668284 ,  2.67468251, -0.08890863],
+            [ 0.75710073,  1.64514855, -1.49304881],
+            [-0.08449747,  1.05468084,  2.74085508],
+            [-1.08888363, -1.94654662,  0.42889803]]
+
+        var train_labels = [0, 0, 2, 0, 2, 1, 1, 1, 1, 2, 1, 0, 1, 0]
+        var test_labels = [2, 0, 0, 2, 1]
+
+        for(var i = 0; i < train_examples.length; i++) {
+            randomforest.addExample(train_examples[i], train_labels[i]);
+        }
+
+        randomforest.train();
+        // the weak learner is especially non-deterministic as the tree is trained on the dot product of 2 randomly selected dimensions
+        var count = 0;
+        for(var tests=0; tests<200; tests++){
+            randomforest.train();
+            if(randomforest.classify([1.0, 2.0, 3.0]) == 0) {
+                count++;
+            }
         }
         expect(count).toBeGreaterThan(50);
 		});
-	 });
+
+});
